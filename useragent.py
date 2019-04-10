53fea67b5a361aad1f1ce2ef6332a01865844eac
Saisyam <saisyam@saisyam.com>
2019-04-10 16:57:28 +0530

    added Opera browser useragents

--- useragent.py
+++ useragent.py
@@ -47,6 +47,20 @@ class UserAgent:
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586",
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
         ]
+        self.opera = [
+            "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
+            "Opera/9.80 (Macintosh; Intel Mac OS X 10.14.1) Presto/2.12.388 Version/12.16",
+            "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14",
+            "Mozilla/5.0 (Windows NT 6.0; rv:2.0) Gecko/20100101 Firefox/4.0 Opera 12.14",
+            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14",
+            "Opera/12.80 (Windows NT 5.1; U; en) Presto/2.10.289 Version/12.02",
+            "Opera/9.80 (Windows NT 6.1; U; es-ES) Presto/2.9.181 Version/12.00",
+            "Opera/12.0(Windows NT 5.2;U;en)Presto/22.9.168 Version/12.00",
+            "Opera/12.0(Windows NT 5.1;U;en)Presto/22.9.168 Version/12.00",
+            "Mozilla/5.0 (Windows NT 5.1) Gecko/20100101 Firefox/14.0 Opera/12.0",
+            "Opera/9.80 (Windows NT 6.1; WOW64; U; pt) Presto/2.10.229 Version/11.62",
+            "Opera/9.80 (Windows NT 6.0; U; pl) Presto/2.10.229 Version/11.62"
+        ]
     
     def get_user_agent(self, browser):
         ua = self.chrome[0]
