651ab8b0c11b9c1e5930d2bb4583d1c0b8851408
Saisyam <saisyam@saisyam.com>
2019-04-28 08:55:52 +0530

    filter proxies by port

--- getproxies.py
+++ getproxies.py
@@ -11,26 +11,26 @@ class GetProxies:
         self.getproxylisturl = "https://api.getproxylist.com/proxy"
         self.proxyscrapeurl = "https://api.proxyscrape.com"
         
-    def getfreeproxies(self, type, country):
+    def getfreeproxies(self, type, country, port=None):
         choice = random.randint(1,3)
         proxies = []
         if choice == 1:
-            proxies = self.get_proxies(type, country)
+            proxies = self.get_proxies(type, country, port)
         elif choice == 2:
-            proxies = self.getproxylist(type, country)
+            proxies = self.getproxylist(type, country, port)
         elif choice == 3:
-            proxies =  self.proxyscrape(type, country)
+            proxies =  self.proxyscrape(type, country, port)
         
         if proxies is None and choice == 1:
-            proxies = self.getproxylist(type, country)
+            proxies = self.getproxylist(type, country, port)
         if proxies is None and choice == 2:
-            proxies = self.proxyscrape(type, country)
+            proxies = self.proxyscrape(type, country, port)
         if proxies is None and choice == 3:
-            proxies = self.get_proxies(type, country)
+            proxies = self.get_proxies(type, country, port)
 
         return proxies
 
-    def get_proxies(self, type, country):
+    def get_proxies(self, type, country, port=None):
         url = self.url+"?type="+type+"&country="+country+"&anon=elite"
         r = requests.get(url)
         proxies = []
@@ -42,12 +42,14 @@ class GetProxies:
                 if len(data) == 2:
                     proxy["ip"] = data[0]
                     proxy["port"] = data[1]
+                    if port is not None and port == data[1]:
+                        proxies.append(proxy)
                     proxies.append(proxy)
             return proxies
         else:
             return None
     
-    def getproxylist(self, type, country):
+    def getproxylist(self, type, country, port=None):
         url = self.getproxylisturl+"?anonymity[]=high%20anonymity&anonymity[]=transparent&maxConnectTime=1&country[]="+country
         if type=="https":
             url = url + "&allowHttps=1"
@@ -58,12 +60,14 @@ class GetProxies:
             proxy_data = json.loads(r.text)
             proxy["ip"] = proxy_data['ip']
             proxy["port"] = proxy_data['port']
+            if port is not None and port == proxy['port']:
+                proxies.append(proxy)
             proxies.append(proxy)
             return proxies
         else:
             return None
     
-    def proxyscrape(self, type, country):
+    def proxyscrape(self, type, country, port=None):
         url = self.proxyscrapeurl+"?request=getproxies&proxytype=http&timeout=5000&anonymity=elite&country="+country
         if type=="https":
             url = url + "&ssl=yes"
@@ -77,6 +81,8 @@ class GetProxies:
                 if len(data) == 2:
                     proxy["ip"] = data[0]
                     proxy["port"] = data[1]
+                    if port is not None and port == data[1]:
+                        proxies.append(proxy)
                     proxies.append(proxy)
             return proxies
         else:
